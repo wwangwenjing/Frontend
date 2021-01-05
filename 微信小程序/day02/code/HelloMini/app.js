184c9ce6bdfaf641528407929db3d88d7b2e0615
wwangwenjing <3206796951@qq.com>
2021-01-05 20:36:39 +0800

    今日内容

---
+++ 微信小程序/day02/code/HelloMini/app.js
@@ -0,0 +1,15 @@
+//app.js
+App({
+ onLaunch(){
+  console.log("小程序启动的时候")
+ },
+ onShow(){
+  console.log("小程序显示的时候")
+ },
+ onHide(){
+  console.log("小程序隐藏的时候")
+ },
+ onError(){
+  console.log("小程序出错的时候")
+ }
+})
